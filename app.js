f4ab5a3fa72d911ebfdeb3941e31965577f8bd7f
iner-17 <inercollado52@gmail.com>
2020-08-14 16:51:05 +0900

    animated menu icon

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${ index / 7 + 0.7 }s`
             }
         });
+        burger.classList.toggle('toggle')
     })
 }
 
